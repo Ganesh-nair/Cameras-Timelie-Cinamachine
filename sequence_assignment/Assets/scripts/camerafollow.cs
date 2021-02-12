0ff3228fe063966f700672883fc3b82845aa4080
Ganesh-nair <ganesh.nair.2022@dicevfs.co.in>
2021-02-12 20:15:47 +0530

    sequence_done

---
+++ sequence_assignment/Assets/scripts/camerafollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    public Transform player;
+    public float smoothFollow;
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position,player.position,Time.deltaTime * smoothFollow);
+    }
+}
